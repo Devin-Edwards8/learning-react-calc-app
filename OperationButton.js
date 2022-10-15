9aed0537ff199bac1cbd9c415454fe739f5d8869
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2022-10-15 15:26:31 -0400

    init

---
+++ OperationButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App"
+
+export default function OperationButton({ dispatch, operation }) {
+    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: {operation}})}>{operation}</button>
+}
